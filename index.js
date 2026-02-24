fe03c19fccdecf9cf20f03fafb3369e9ee03b109
Luis <58019574+ludev84@users.noreply.github.com>
2026-02-23 19:19:59 -0600

    Basic functionality using only objects

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
-const Gameboard = {
-  gameboard: [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ],
-};
+function Player(name, marker) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.name = name;
+  this.marker = marker;
+}
 
-const Player = {
-  playerOne: {
-    name: "tim",
-    marker: "X",
-  },
-  playerTwo: {
-    name: "Jenn",
-    marker: "O",
-  },
-};
-
-const Game = {
-  printGameboard: function (board) {
+function Game() {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.gameboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  this.play = function (player, pos) {
+    const row = pos[0] - 1;
+    const col = pos[1] - 1;
+    const currentCell = this.gameboard[row][col];
+    if (currentCell !== "") {
+      throw Error("You must select an empty cell");
+    }
+    this.gameboard[row][col] = player.marker;
+    console.log(this.printGameboard(this.gameboard));
+    const gameFinished = this.isGameFinished(this.gameboard);
+    if (gameFinished) {
+      console.log("Game finished");
+    }
+  };
+  this.printGameboard = function (board) {
     return board
       .map(
         (row) =>
           " " + row.map((cell) => (cell === "" ? " " : cell)).join(" | ") + " ",
       )
       .join("\n---+---+---\n");
-  },
-};
+  };
+  this.isGameFinished = function () {
+    for (let row of this.gameboard) {
+      for (let cell of row) {
+        if (cell === "") return false;
+      }
+    }
+    return true;
+  };
+}
 
-console.log(Game.printGameboard(Gameboard.gameboard));
+const game = new Game();
+const player1 = new Player("Joe", "X");
+const player2 = new Player("Karen", "O");
+
+game.play(player1, [1, 2]);
+game.play(player2, [1, 3]);
+game.play(player1, [1, 1]);
+game.play(player2, [2, 1]);
+game.play(player1, [2, 2]);
+game.play(player2, [3, 3]);
+game.play(player1, [3, 1]);
+game.play(player2, [3, 2]);
+game.play(player1, [2, 3]);
