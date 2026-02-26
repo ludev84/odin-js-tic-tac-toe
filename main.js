538dfa62343d4fe67f14a448bd6550e6b74fb49d
Luis <58019574+ludev84@users.noreply.github.com>
2026-02-25 22:56:07 -0600

    Basic functionality with factory functions

---
+++ main.js
@@ -0,0 +1,70 @@
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push("");
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeToken = (position, marker) => {
+    const row = position[0] - 1;
+    const col = position[1] - 1;
+    const currentCell = board[row][col];
+    if (currentCell !== "") return;
+    board[row][col] = marker;
+  };
+
+  const printGameboard = () => {
+    const consoleBoard = board
+      .map(
+        (row) =>
+          " " + row.map((cell) => (cell === "" ? " " : cell)).join(" | ") + " ",
+      )
+      .join("\n---+---+---\n");
+    console.log(consoleBoard);
+  };
+
+  return { getBoard, placeToken, printGameboard };
+}
+
+function GameController(playerOneName = "Joe", playerTwoName = "Karen") {
+  const players = [
+    {
+      name: playerOneName,
+      marker: "X",
+    },
+    {
+      name: playerTwoName,
+      marker: "O",
+    },
+  ];
+
+  const board = Gameboard();
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const playRound = (position) => {
+    board.placeToken(position, getActivePlayer().marker);
+    board.printGameboard();
+    switchPlayerTurn();
+  };
+
+  return { playRound, getActivePlayer };
+}
+
+const game = GameController();
+
+// game.playRound([1,3])
+// game.playRound([1,2])
